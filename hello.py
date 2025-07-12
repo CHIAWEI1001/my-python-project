bf4bac1107826774b228b076e3a10a8fde8584bd
CHIAWEI CHU <44937908-chiaweichu@users.noreply.replit.com>
2025-07-12 14:53:32 +0000

    Fix Python syntax errors

--- hello.py
+++ hello.py
@@ -1,1 +1,5 @@
-print("Hello, 現在可以在任何地方寫程式了！")\nprint("這是在 Replit 上修改的版本")\nprint("Hello, 這是在家中電腦修改的版本！")\nprint("現在測試 Git 同步功能")
+print("Hello, 現在可以在任何地方寫程式了！")
+print("這是在 Replit 上修改的版本")
+print("Hello, 這是在家中電腦修改的版本 1")
+print("現在測試 Git 同步功能")
+
